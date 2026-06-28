008040c587f804518c885e4b13edb7a1d290691e
pull <pull@users.noreply.github.com>
2026-06-28 06:54:07 +0000

    deps: update libarchive to v3.8.8 (7219b0134d771dc4b51bf86b4d01761b87398b1b)

--- cmake/targets/BuildLibArchive.cmake
+++ cmake/targets/BuildLibArchive.cmake
@@ -4,7 +4,7 @@ register_repository(
   REPOSITORY
     libarchive/libarchive
   COMMIT
-    7118f97c26bf0b2f426728b482f86508efc81d02
+    7219b0134d771dc4b51bf86b4d01761b87398b1b
 )
 
 register_cmake_command(
